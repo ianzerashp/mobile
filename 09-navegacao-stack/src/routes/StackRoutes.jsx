e5dde2e55592ddcd6eaa1376c6a2d889502f0cb0
ianzerashp <ianbarros77@gmail.com>
2025-05-06 20:27:38 -0300

    Aula 06/05

--- 09-navegacao-stack/src/routes/StackRoutes.jsx
+++ 09-navegacao-stack/src/routes/StackRoutes.jsx
@@ -3,6 +3,9 @@ import HomeScreen from '../screens/HomeScreen'
 import ProfileScreen from '../screens/ProfileScreen'
 import ConfigScreen from '../screens/ConfigScreen'
 
+import ListaScreen from '../screens/ListaScreen'
+import ItemScreen from '../screens/ItemScreen'
+
 // import do stack
 import { createStackNavigator } from "@react-navigation/stack"
 
@@ -12,6 +15,9 @@ const Stack = createStackNavigator()
 export default function StackRoutes() {
   return (
     <Stack.Navigator>
+
+      <Stack.Screen name='ListaScreen' component={ListaScreen} />
+      <Stack.Screen name='ItemScreen' component={ItemScreen} />
 
       <Stack.Screen
         name='HomeScreen'
